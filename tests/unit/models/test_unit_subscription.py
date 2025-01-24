38a3aace7f3bd3c97a7d257379d6b708bdf18d12
PaperMtn <andrew.byford11+github@gmail.com>
2025-01-24 19:56:35 +0000

    Handle refactor of subscriptions in config file

--- tests/unit/models/test_unit_subscription.py
+++ tests/unit/models/test_unit_subscription.py
@@ -9,18 +9,19 @@ def test_subscription_initialisation(mock_subscription):
 
     # Test that the subscription object has the correct attributes
     assert mock_subscription.subscription_type == 'category'
-    assert mock_subscription.name == 'cloud security'
-    assert mock_subscription.rank == 20
-    assert mock_subscription.resource_types == ['post', 'news']
-    assert mock_subscription.users == ['tobias.funke@example.com']
-    assert mock_subscription.channels == ['C01234567']
+    assert mock_subscription.query == 'cloud security'
+    assert mock_subscription.filters.rank == 20
+    assert mock_subscription.filters.resource_types == ['post', 'news']
+    assert mock_subscription.slack_destinations.users == ['tobias.funke@example.com']
+    assert mock_subscription.slack_destinations.channels == ['C01234567']
 
 
 def test_subscription_creation_missing_fields():
     # Test that a Subscription object is created correctly with missing fields
     subscription_dict = {
         'subscription_type': 'category',
-        'name': 'firefox',
+        'id': 'firefox',
+        'query': 'firefox'
     }
 
     mock_subscription = subscription.create_subscription_from_dict(subscription_dict)
@@ -29,20 +30,24 @@ def test_subscription_creation_missing_fields():
 
     # Test default values are set correctly
     assert mock_subscription.subscription_type == 'category'
-    assert mock_subscription.name == 'firefox'
-    assert mock_subscription.rank == 80.00
-    assert mock_subscription.resource_types == ['post', 'news', 'oss', 'video', 'paper', 'slides', 'n_a']
-    assert not mock_subscription.curated
-    assert mock_subscription.users is None
-    assert mock_subscription.channels is None
+    assert mock_subscription.query == 'firefox'
+    assert mock_subscription.filters.rank == 80.00
+    assert mock_subscription.filters.resource_types == ['post', 'news', 'oss', 'video', 'paper', 'slides', 'n_a']
+    assert not mock_subscription.filters.curated
+
+    # Slack config should be None
+    assert not mock_subscription.slack_destinations
 
 
 def test_subscription_creation_invalid_rank():
     # Test that a Subscription object is created correctly with invalid rank
     subscription_dict = {
         'subscription_type': 'category',
-        'name': 'firefox',
-        'rank': 'invalid'
+        'id': 'firefox',
+        'query': 'firefox',
+        'filters': {
+            'rank': 'invalid'
+        }
     }
 
     subscription_object = subscription.create_subscription_from_dict(subscription_dict)
@@ -50,18 +55,23 @@ def test_subscription_creation_invalid_rank():
     assert isinstance(subscription_object, subscription.Subscription)
 
     # Test rank is correctly set to its default value
-    assert subscription_object.rank == 80.00
+    assert subscription_object.filters.rank == 80.00
 
 def test_subscription_creation_incorrect_field_type():
     # Test that a Subscription object is created correctly with incorrect field type
     subscription_dict = {
         'subscription_type': 'category',
-        'name': 'firefox',
-        'rank': 'invalid',
-        'resource_types': 'post',
-        'curated': 'True',
-        'users': 123,
-        'channels': 123
+        'id': 'firefox',
+        'query': 'firefox',
+        'filters': {
+            'rank': 'invalid',
+            'resource_types': 'post',
+            'curated': 'True'
+        },
+        'slack_destinations': {
+            'users': 123,
+            'channels': 123
+        }
     }
 
     with pytest.raises(TypeError):
